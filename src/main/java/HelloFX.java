065e966c70f28f4068378f69fdd2fbaf9fc79448
Szymon Wojciechowski <infero93.ulx@gmail.com>
2019-02-25 11:41:34 +0100

    Wczytywanie obrazków zapisanych w jarze

--- src/main/java/HelloFX.java
+++ src/main/java/HelloFX.java
@@ -3,17 +3,19 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import pl.techplayground.GameMechanic;
 
 public class HelloFX extends Application {
 
     @Override
     public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+//        String javaVersion = System.getProperty("java.version");
+//        String javafxVersion = System.getProperty("javafx.version");
+//        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+//        Scene scene = new Scene(new StackPane(l), 640, 480);
+//        stage.setScene(scene);
+//        stage.show();
+        new GameMechanic().initializeGrid();
     }
 
     public static void main(String[] args) {
--- src/main/java/pl/techplayground/GameMechanic.java
+++ src/main/java/pl/techplayground/GameMechanic.java
@@ -1,22 +1,34 @@
 package pl.techplayground;
 
+import javafx.scene.image.Image;
 import pl.techplayground.model.MemoryTile;
 
 public class GameMechanic {
     private static final Integer GRID_WIDTH = 3;
     private static final Integer GRID_HEIGHT = 3;
+    private static final Integer IMAGES = 3;
 
     private MemoryTile[][] tiles;
+    private Image[] images;
 
     public GameMechanic() {
         tiles = new MemoryTile[GRID_WIDTH][GRID_HEIGHT];
+        images = new Image[IMAGES];
     }
 
     public void initializeGrid() {
+        getImagesFromResources();
+
         for(int x = 0; x < GRID_WIDTH; x++) {
             for(int y = 0; y < GRID_HEIGHT; y++) {
                 tiles[x][y] = new MemoryTile(null);
             }
         }
+    }
+
+    private void getImagesFromResources() {
+        images[0] = new Image("/bird.png");
+        images[1] = new Image("/dog.png");
+        images[2] = new Image("/fish.png");
     }
 }
